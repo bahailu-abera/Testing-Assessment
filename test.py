f79ba3e82fafd8ee8b7fd89e44c6f60517a52755
Bahailu Abera <aberabahailu4@gmail.com>
2023-03-27 17:12:30 +0300

    test to_upper method

--- test.py
+++ test.py
@@ -1,17 +1,31 @@
+"""
+Test module for string module
+"""
 import unittest
 from string_functions import *
 
+
 class TestToUpper(unittest.TestCase):
-    # write your code here
-    pass
+
+    def test_to_upper(self):
+        """
+        Test to_upper method of the string module
+        """
+        self.assertEqual(to_upper('hello'), 'HELLO')
+        self.assertEqual(to_upper('Hello'), 'HELLO')
+        self.assertEqual(to_upper(''), '')
+        self.assertEqual(to_upper('helLO2423!'), 'HELLO2423!')
+
 
 class TestToLower(unittest.TestCase):
-    # Write you code here
+    # your code goes here
     pass
 
+
 class TestCapitalize(unittest.TestCase):
-    # Write your code here
+    # your code goes here
     pass
+
 
 if __name__ == '__main__':
     unittest.main()
